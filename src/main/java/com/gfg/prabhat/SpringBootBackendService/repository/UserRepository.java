1c2ada9f1b1e99bb87a24db8c9c4f3a4a1cc07bd
Prabhat Singh Tomar <iampstomar@gmail.com>
2019-06-21 22:49:57 +0530

    Updated DaoService

--- src/main/java/com/gfg/prabhat/SpringBootBackendService/repository/UserRepository.java
+++ src/main/java/com/gfg/prabhat/SpringBootBackendService/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.gfg.prabhat.SpringBootBackendService.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
